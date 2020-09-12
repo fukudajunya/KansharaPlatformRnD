2c39cb0968114478be0162ce09c15cb6404aacb5
fukudajunya <f.econ.j.1019@gmail.com>
2020-09-13 00:50:37 +0900

    機能追加

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,4 @@
-// This is a JavaScript file
+// Mobile Backend config
 const applicationKey = 'f7567c6df41b6c1f2ac9d37ff4c726214f70d4e3a94b51df5859b27b5bb8053f';
 const clientKey = '32b5cf5d98fbc11a9cf9ad16499e7b0f7c46e7a78404582d150ec4caf12f89a5';
 const applicationId = 'rwIDQxFJnkviPVqJ';
@@ -19,67 +19,66 @@ document.addEventListener('init', function (event) {
     if (!user) {
       // ログインページを表示
       document.querySelector('#navigator').pushPage('register.html', { animation: 'fade' });
-    }else{
+    } else {
       document.querySelector('#navigator').pushPage('main.html', { animation: 'fade' });
     }
   }
 });
 
-// ユーザ登録/ログイン処理です
+// ユーザ登録/ログイン処理
 const login = () => {
-  // 入力された情報です
   const userName = document.getElementById('username').value;
   const password = document.getElementById('password').value;
-  // ユーザを作成します
   const user = new ncmb.User();
   user
     .set("userName", userName)
     .set("password", password);
 
-    // 登録処理を実行します
-    user.signUpByAccount()
-      .then(() => {
-      // 成功したらログイン処理を行います
+  // 登録処理
+  user.signUpByAccount()
+    .then(() => {
+      // 初回ログイン
       console.log("登録成功");
       return ncmb.User.login(userName, password)
     })
     .catch((err) => {
-      // 失敗したらログイン処理を行います
-      console.log("登録失敗。ログイン試行開始")
+      // 通常時ログイン
+      console.log("ログイン試行開始")
       return ncmb.User.login(userName, password)
     })
     .then((user) => {
-      // ログイン成功したらメイン画面に遷移します
+      // ログイン後、ページ遷移
       console.log("ページ移動")
-      document.querySelector('#navigator').pushPage('main.html', {animation: 'fade'});
+      document.querySelector('#navigator').pushPage('main.html', { animation: 'fade' });
     })
     .catch((err) => {
-      // 失敗したらアラートを出します
-      ons.notification.alert('Login failed.')
+      ons.notification.alert({
+        title: 'ログイン失敗',
+        message: 'ユーザー名またはパスワードが間違っています。'})
     });
 };
 
-// ログアウト処理です
+// ログアウト処理
 const logout = () => {
-  // 確認ダイアログを出します
   ons.notification.confirm({
-    message: 'Are you sure?'
+    title: '',
+    message: 'ログアウトしますか？'
   })
-  .then((id) => {
-    // id == 1 はOKボタンを押した場合です
-    if (id != 1) {
-      throw 1;
-    }
-    // ログアウト処理
-    return ncmb.User.logout();
-  })
-  .then(() => {
-    // 処理完了したら登録/ログイン画面に遷移します
-    document.querySelector('#navigator').pushPage('register.html', {animation: 'fade'});
-  })
-  .catch(() => {
-    // 確認ダイアログでCancelを選んだ場合
-  })
+    .then((id) => {
+      // id == 1 はOKボタンを押した場合です
+      if (id != 1) {
+        throw 1;
+      }
+      // ログアウト処理
+      return ncmb.User.logout();
+    })
+    .then(() => {
+      // 登録/ログイン画面に遷移
+      document.querySelector('#navigator').pushPage('register.html', { animation: 'fade' });
+    })
+    .catch(() => {
+      // 確認ダイアログでCancelを選んだ場合
+    })
 };
 
 // 入力した文字をデータストアに格納する
@@ -92,7 +91,8 @@ function send_to_datastore(text) {
   });
 }
 
-// 画像クリック時にダイアログを出現させる
+/* アイテム一覧 */
+// 画像クリック時
 function imageClick() {
   var dialog = document.getElementById('my-dialog');
   if (dialog) {
@@ -111,4 +111,11 @@ function hideDialog(id) {
     .getElementById(id)
     .hide();
 }
-
+/* 持ち物確認 */
+// 準備完了
+function checkComplete() {
+  ons.notification.alert({
+    title: '',
+    message: '行ってらっしゃい！'
+  });
+}
